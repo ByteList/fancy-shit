2a707dd1a2424ab4f0db45391114c3b3257aaf70
ByteList <n.emmrich@wrba.ch>
2019-09-04 13:32:17 +0200

    Added fancy-shit

---
+++ fancy-shit/src/main/java/de/niklasemm/school/fancyshit/FancyShitLauncher.java
@@ -0,0 +1,13 @@
+package de.niklasemm.school.fancyshit;
+
+/**
+ * Created by nemmerich on 02.09.2019.
+ * <p>
+ * Copyright by nemmerich - https://bytelist.de/
+ */
+public class FancyShitLauncher {
+
+    public static void main(String[] args) {
+
+    }
+}
